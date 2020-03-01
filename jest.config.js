566efb765a57399fa6d888a70ffa8054404ff84d
lrgfilipe <luis.filipe@linkconsulting.com>
2020-03-01 14:15:10 +0000

    WIP

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,5 @@ module.exports = {
   },
   "collectCoverage": true,
   "collectCoverageFrom": ["src/components/*.vue"], // ["**/*.{js,vue}"],
-  "coverageReporters": ["text-summary", "json-summary"]
+  "coverageReporters": ["text-summary", "json-summary", "html", "json"]
 }
